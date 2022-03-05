50b543714d646c5a7ef2b4475302b81b8d8c7eb0
QianhaoLiu <ql2043@nyu.edu>
2022-03-04 21:33:23 -0500

    再次试试

--- src/main/java/com/nowcoder/community/config/AlphaConfig.java
+++ src/main/java/com/nowcoder/community/config/AlphaConfig.java
@@ -9,6 +9,7 @@ import java.text.SimpleDateFormat;
 @Configuration
 public class AlphaConfig {
 
+    //this is a submit test
     @Bean
     public SimpleDateFormat simpleDateFormate(){
         return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
